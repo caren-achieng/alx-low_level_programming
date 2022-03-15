835bfe4ab14125f1a45ad7273dc7ae9582096b74
Caren Achieng <75131814+caren-achieng@users.noreply.github.com>
2022-03-15 19:45:23 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,17 @@
 #include "main.h"
-
 /**
  * main - Entry point
- * @c: The character to print
  *
  * Return: Always 0 (Success).
  */
 int main(void)
 {
 	char *str = "_putchar";
-		
 	while (*str)
 	{
 		_putchar(*str);
 		str++;
 	}
 	_putchar('\n');
-	
 	return (0);
 }
