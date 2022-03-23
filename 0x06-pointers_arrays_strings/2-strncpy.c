df027ff1fa158247f288bdb7435b2781740e528d
Caren Achieng <75131814+caren-achieng@users.noreply.github.com>
2022-03-23 20:13:50 +0300

    Update 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -4,7 +4,7 @@
  * @dest: destination.
  * @src: source.
  * @n: amount of bytes from src.
- * Return : the pointer to dest.
+ * Return: the pointer to dest.
  */
 
 char *_strncpy(char *dest, char *src, int n)
