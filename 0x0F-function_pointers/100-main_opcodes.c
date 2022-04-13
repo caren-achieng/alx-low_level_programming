16d13fab1e5b8bdf955d3f471b43ed18010fed9f
caren-achieng <caren.achieng@strathmore.edu>
2022-04-13 21:51:31 +0300

    added the #

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,40 +1,49 @@
-include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
- * main - check the code for Holberton School students.
- * @argc: argument count.
- * @argv: argument vector.
+ * print_opcodes - print the opcodes of this program
+ * @a: address of the main function
+ * @n: number of bytes to print
  *
- * Return: Always 0.
+ * Return: void
  */
-int main(int argc, char *argv[])
+void print_opcodes(char *a, int n)
 {
-        char *opc = (char *) main;
-        int i, nbytes;
+	int i;
 
-        if (argc != 2)
-        {
-                printf("Error\n");
-                exit(1);
-        }
+	for (i = 0; i < n; i++)
+	{
+		printf("%.2hhx", a[i]);
+		if (i < n - 1)
+			printf(" ");
+	}
+	printf("\n");
 
-        nbytes = atoi(argv[1]);
-
-        if (nbytes < 0)
-        {
-                printf("Error\n");
-                exit(2);
-        }
-
-        for (i = 0; i < nbytes; i++)
-        {
-                printf("%02x", opc[i] & 0xFF);
-                if (i != nbytes - 1)
-                        printf(" ");
-        }
-
-        printf("\n");
-        return (0);
 }
 
+/**
+ * main - prints the opcodes of its own main function
+ * @argc: number of arguments passed to the function
+ * @argv: array of pointers to arguments
+ *
+ * Return: always O
+ */
+int main(int argc, char **argv)
+{
+	int n;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	n = atoi(argv[1]);
+	if (n < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	print_opcodes((char *)&main, n);
+	return (0);
+}
