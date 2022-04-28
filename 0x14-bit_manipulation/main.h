e8465373b41b79846f8e11cf97f2ef5bb56c7d48
caren-achieng <caren.achieng@strathmore.edu>
2022-04-28 22:31:43 +0300

    ugh

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,9 +1,9 @@
-#ifndef _MAIN_
-#define _MAIN__
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
+int _putchar(char);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
-int _putchar(char c);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
